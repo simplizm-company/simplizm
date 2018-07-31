b69f27998f97e0bb7cdf117b65aa779f6291ea82
shiniyongjun <shinyj1991@gmail.com>
2018-07-31 23:53:35 +0900

    asdf
    asdf

--- src/components/include/CommonHeader.vue
+++ src/components/include/CommonHeader.vue
@@ -12,8 +12,8 @@ export default {
 </script>
 
 <style lang="scss">
-#header {display: flex; justify-content: space-between; align-items: center; position: fixed; top: 10px; right: 10px; left: 10px; z-index: 100; height: 80px; padding: 0 30px; margin: 0 auto; border: 1px solid #000000;
-    .logo {font-family: NanumSquare; font-weight: 700; font-size: 30px; color: #000000;}
+#header {display: flex; justify-content: space-between; align-items: center; max-width: 1000px; height: 80px; padding: 0 30px; margin: 10px auto; border: 1px solid #000000;
+    .logo {font-family: NanumSquare; font-weight: 700; font-size: 32px; color: #000000;}
     .gnb-toggle {position: relative; width: 50px; height: 28px;
         &:before {display: block; content: ''; position: absolute; top: 0; left: 0; width: 50px; height: 4px; border: 1px solid #000000;}
         div {position: absolute; top: 12px; left: 0; width: 100%; height: 4px; border: 1px solid #000000; text-indent: -9999em;}
